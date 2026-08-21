3e2a711bd9caa09003088f6f1d43f61898aec53e
ibroximxudoyqulov <ibroximxudoyqulov10@gmail.com>
2026-08-21 15:27:09 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -193,15 +193,16 @@ def message_handler(message):
         bot.send_message(message.chat.id, TEXTS[lang]["select_lang"], reply_markup=markup)
         return
 
-if text in [TEXTS["uz"]["btn_info"], TEXTS["ru"]["btn_info"]]:
+# BOT HAQIDA TUGMASI BOSILGANDA (JONLI UNIKAL STATISTIKA)
+    if text in [TEXTS["uz"]["btn_info"], TEXTS["ru"]["btn_info"]]:
         total = get_total_users()
         info_msg = (
-            f"🌟 <b>VibeStudio & VibeMusic</b> — TikTok va Instagram uchun estetik Spotify videolarni va musiqalarni 1 daqiqada tayyorlab beruvchi bepul platforma!\n\n"
-            f"👥 <b>Foydalanuvchilar soni:</b> <code>{total}</code> ta obunachi\n"
+            f"🌟 <b>VibeStudio & VibeMusic</b> — TikTok va Instagram uchun estetik Spotify & WhatsApp videolarni 1 daqiqada tayyorlab beruvchi bepul platforma!\n\n"
+            f"👥 <b>Foydalanuvchilar soni:</b> <code>{total}</code> ta unikal obunachi\n"
             f"⚡️ <b>Platforma holati:</b> 24/7 Bepul & Faol\n\n"
             f"👨‍💻 Dasturchi & Admin: @IBROXIM_I6"
             if lang == 'uz' else
-            f"🌟 <b>VibeStudio & VibeMusic</b> — бесплатная платформа для создания эстетичных Spotify видео для TikTok и Instagram за 1 минуту!\n\n"
+            f"🌟 <b>VibeStudio & VibeMusic</b> — бесплатная платформа для создания эстетичных Spotify & WhatsApp видео для TikTok и Instagram за 1 минуту!\n\n"
             f"👥 <b>Количество пользователей:</b> <code>{total}</code>\n"
             f"⚡️ <b>Статус платформы:</b> 24/7 Бесплатно & Активно\n\n"
             f"👨‍💻 Разработчик: @IBROXIM_I6"
